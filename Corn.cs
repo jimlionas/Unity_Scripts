4e3c33dd84e2c332e849e2f021c8c5a587eeda5b
Jim Lionas <75275930+jimlionas@users.noreply.github.com>
2025-07-08 10:58:05 +0300

    Add files via upload

---
+++ Corn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corn : MonoBehaviour
+{
+    public static Corn singleton;
+    public int health;
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+
+    public void TakeDamage()
+    {
+        if(health > 0)
+        {
+            health -= 1;
+        }
+    }
+
+}
